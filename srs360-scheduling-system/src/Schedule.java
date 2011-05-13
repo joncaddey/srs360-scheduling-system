858d0308e8ed1afd7c7d8806c7f7acb9ddeaca4b
joncaddey@gmail.com <joncaddey@gmail.com@51c3290b-bf2c-f903-1b12-c83ff7bf9553>
2011-05-13 20:07:09 +0000

    Changed max line length from 80 to 60.  Changed comment template to have line break before invariants and preconditions.  Added Schedule.

---
+++ srs360-scheduling-system/src/Schedule.java
@@ -0,0 +1,24 @@
+/**
+ * Simple Random Sample
+ * 
+ * TCSS 360 Dr. Tenenberg
+ * 
+ * srs360-scheduling-system
+ */
+
+/**
+ * Represents a Schedule within the program. <br>
+ * <br>
+ * <b>Invariants:</b>
+ * <ul>
+ * <li>invariant1</li>
+ * </ul>
+ * 
+ * @author Jonathan Caddey
+ * @version May 13, 2011
+ */
+public class Schedule
+{
+
+
+}
