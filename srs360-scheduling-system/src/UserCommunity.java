c051a4c975b568703c66a8f04061ebda3e397da4
cloutier.gman <cloutier.gman@gmail.com>
2011-05-13 19:30:09 +0000

    added UserCommunity.
    For further comments please follow this style:
        use verb subject phrases for commit comments

---
+++ srs360-scheduling-system/src/UserCommunity.java
@@ -0,0 +1,37 @@
+/**
+ * Simple Random Sample
+ *
+ * TCSS 360 Dr. Tenenberg
+ *
+ * srs360-scheduling-system
+ */
+
+/**
+ * 
+ * <b>Invariants:</b>
+ * <ul><li>invariant1</li>
+ * </ul>
+ * 
+ * @author Gregory Cloutier
+ * @version May 13, 2011
+ */
+public class UserCommunity
+{
+  /**
+   * <b>Preconditions:</b>
+   * <ul><li>pre1</li>
+   * <li>pre2</li>
+   * </ul>
+   * <b>Postconditions:</b>
+   * <ul><li>post1</li>
+   * <li>post2</li>
+   * </ul>
+   * @param userID
+   * @param password
+   * @return
+   */
+  public boolean authenticate(final String userID, final String password)
+  {
+    return false;
+  }
+}
