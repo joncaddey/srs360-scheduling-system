ea0635e3357d4cdb3a04c1b485d5af0945bef6e9
joncaddey@gmail.com <joncaddey@gmail.com@51c3290b-bf2c-f903-1b12-c83ff7bf9553>
2011-05-14 05:42:16 +0000

    fixed format so it enforces 60 column width when using ctr1+shift+f.  Added fields to Course.

--- srs360-scheduling-system/src/Course.java
+++ srs360-scheduling-system/src/Course.java
@@ -7,8 +7,9 @@
  */
 
 /**
- * Contains information about a course like course title, level, etc., but not
- * information about a specific section like who teaches it. <br>
+ * Contains information about a course like course title,
+ * level, etc., but not information about a specific section
+ * like who teaches it. Courses are immutable.<br>
  * <br>
  * <b>Invariants:</b>
  * <ul>
@@ -20,5 +21,40 @@
  */
 public class Course
 {
+  /**
+   * Example: "TCSS". No number is included.
+   */
+  private final String my_curriculum_code;
+
+  /**
+   * The number for the course; for example, the 360 in
+   * "TCSS 360".
+   */
+  private final int my_level;
+
+  /**
+   * The name of the course, as it appears on the schedule.
+   * Example: "INTR OBJ-ORIENT PRG".
+   */
+  private final String my_title;
+
+  /**
+   * Creates a Course. For example, in
+   * "TCSS 360 SOFTWARE DEV & QA" "TCSS" is the curriculum
+   * code, 360 is the course level, and "SOFTWARE DEV & QA"
+   * is the title.
+   * 
+   * @param the_curriculum_code the curriculum code of the
+   *          course.
+   * @param the_level the level of the course.
+   * @param the_title the title of the course.
+   */
+  public Course(final String the_curriculum_code,
+                final int the_level, final String the_title)
+  {
+    my_curriculum_code = the_curriculum_code;
+    my_level = the_level;
+    my_title = the_title;
+  }
 
 }
