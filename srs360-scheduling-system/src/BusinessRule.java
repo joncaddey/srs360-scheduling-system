e1dbaf19676b2165b60b13e867ebe423da629e8a
joncaddey@gmail.com <joncaddey@gmail.com@51c3290b-bf2c-f903-1b12-c83ff7bf9553>
2011-05-15 20:25:03 +0000

    added more to Schedule, had first merge conflict

--- srs360-scheduling-system/src/BusinessRule.java
+++ srs360-scheduling-system/src/BusinessRule.java
@@ -9,8 +9,8 @@ import java.util.Collection;
  */
 
 /**
- * A BusinessRule checks a Schedule to make sure the Schedule adheres to the
- * BusinessRule. <br>
+ * A BusinessRule checks a Schedule to make sure the
+ * Schedule adheres to the BusinessRule. <br>
  * <br>
  * <b>Invariants:</b> none<br>
  * 
@@ -21,9 +21,12 @@ public interface BusinessRule
 {
 
   /**
-   * Determines whether the given Schedule satisfies the constraints posed by
-   * this BusinessRule. If the constraint is violated, the offending Sections
-   * are returned. Otherwise, an empty Collection is returned. <br>
+   * Determines whether the given Schedule satisfies the
+   * constraints posed by this BusinessRule. If the
+   * constraint is violated, the offending Sections are
+   * returned. Otherwise, an empty Collection is returned.
+   * 
+   * <br>
    * <br>
    * <b>Preconditions:</b>
    * <ul>
@@ -31,13 +34,13 @@ public interface BusinessRule
    * </ul>
    * <b>Postconditions:</b>
    * <ul>
-   * <li>removing the returned sections from the Schedule would satisfy this
-   * constraint.</li>
+   * <li>removing the returned sections from the Schedule
+   * would satisfy this constraint.</li>
    * </ul>
    * 
    * @param the_schedule a valid (non-null) Schedule
-   * @return the list of Sections of the_schedule which keep this constraint
-   *         from being satisfied.
+   * @return the list of Sections of the_schedule which keep
+   *         this constraint from being satisfied.
    */
   Collection<Section> getViolations(Schedule the_schedule);
 }
--- srs360-scheduling-system/src/Schedule.java
+++ srs360-scheduling-system/src/Schedule.java
@@ -6,6 +6,8 @@
  * srs360-scheduling-system
  */
 
+import java.util.Collection;
+
 /**
  * Represents a Schedule within the program. <br>
  * <br>
@@ -19,5 +21,39 @@
  */
 public class Schedule
 {
+  public Schedule(final Collection<Section> the_sections)
+    throws IllegalArgumentException
+  {
 
+  }
+
+  /**
+   * <br>
+   * <br>
+   * <b>Preconditions:</b>
+   * <ul>
+   * <li>pre1</li>
+   * <li>pre2</li>
+   * </ul>
+   * <b>Postconditions:</b>
+   * <ul>
+   * <li>post1</li>
+   * <li>post2</li>
+   * </ul>
+   * 
+   * @param the_courses The Courses that should be on the
+   *          schedule.
+   * @param the_time_preference The times the Courses should
+   *          be offered.
+   * @return which Courses from the_courses are not on the
+   *         Schedule at the given time_preference.
+   */
+  public Collection<Course> getMissingCourses(
+      final Collection<Course> the_courses,
+      final TimePreference the_time_preference)
+  {
+
+    return null;
+
+  }
 }
