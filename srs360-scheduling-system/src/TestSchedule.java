8e612090949cb66181cb619ff6cc28e641c4459a
joncaddey <joncaddey@gmail.com>
2011-05-16 01:45:20 +0000

    finished Schedule API and documentation, more or less

---
+++ srs360-scheduling-system/src/TestSchedule.java
@@ -0,0 +1,57 @@
+
+/**
+ * Simple Random Sample
+ * 
+ * TCSS 360 Dr. Tenenberg
+ * 
+ * srs360-scheduling-system
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+
+/**
+ * Contains a series of tests for the Schedule class.
+ * @author Jonathan Caddey
+ * @version 2011-05-15
+ */
+public class TestSchedule
+{
+  
+  /**
+   * When every Section may meet only on the desired days and
+   * meet only at the desired times, but none are also of the correct Course.
+   */
+  @Test
+  public void testNoCourseHasSection()
+  {
+    assertFalse(false);
+  }
+  
+  
+  /**
+   * For every Section in Course:
+   * Desired Course? yes
+   * Desired Days? yes
+   * Desired Times? no
+   */
+  
+  
+  /**
+   * For every Section in Course:
+   * Desired Course? yes
+   * Desired Days? no
+   * Desired Times? yes
+   */
+  
+  /**
+   * For every Section in Course:
+   * Desired Course? no
+   * Desired Days? yes
+   * Desired Times? yes
+   */
+
+  
+}
