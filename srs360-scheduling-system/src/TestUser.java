4351075b34d486bd4d51d8cc57a8fbbe54150f55
david.liddington@gmail.com <david.liddington@gmail.com@51c3290b-bf2c-f903-1b12-c83ff7bf9553>
2011-05-16 12:54:37 +0000

    Comments are added to User. TestUser has been added to the package; it currently has only a method which tests the getAdvisorPreferences method in the User class.

---
+++ srs360-scheduling-system/src/TestUser.java
@@ -0,0 +1,57 @@
+/**
+ * Simple Random Sample
+ * 
+ * TCSS 360 Dr. Tenenberg
+ * 
+ * srs360-scheduling-system
+ */
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Contains tests for methods in the User class.
+ * 
+ * <br><br><b>Invariants:</b>
+ * <ul><li>invariant1</li>
+ * </ul>
+ * 
+ * @author David
+ * @version May 16, 2011
+ */
+public class TestUser
+{
+  private User user;
+  private AdvisorPreferences advisor_preferences;
+  
+  @Before
+  public void setUp()
+  {
+    user = new User(null);
+    advisor_preferences = new AdvisorPreferences();
+  }
+  
+  /**
+   * <br><br><b>Preconditions:</b>
+   * <ul><li>pre1: a valid Schedule containing TCSS360</li>
+   * </ul>
+   * <b>Postconditions:</b>
+   * <ul><li>post1: verification that TCSS360 is not
+   * included in the List of missing Courses</li>
+   * </ul>
+   * @param the_schedule The Schedule of Courses
+   */
+  @Test
+  public void testGetAdvisorFeedback(Schedule the_schedule)
+  {
+    Course tcss360 = new Course("TCSS360");
+    advisor_preferences.addCourse(tcss360);
+    List<Course> missingCourses = user.getAdvisorFeedback
+                                             (the_schedule);
+    assertFalse(missingCourses.contains(tcss360));   
+  } 
+
+}
