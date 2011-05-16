de221a8c481f06518be0eb0a9c09ec8607c42b20
david.liddington <david.liddington@gmail.com>
2011-05-16 12:54:37 +0000

    Comments are added to User. TestUser has been added to the package; it currently has only a method which tests the getAdvisorPreferences method in the User class.

--- srs360-scheduling-system/src/User.java
+++ srs360-scheduling-system/src/User.java
@@ -1,6 +1,23 @@
+/**
+ * Simple Random Sample
+ * 
+ * TCSS 360 Dr. Tenenberg
+ * 
+ * srs360-scheduling-system
+ */
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Creates a User of the Scheduling System.
+ * 
+ * <br><br><b>Invariants:</b>
+ * <ul><li>invariant1</li>
+ * </ul>
+ * 
+ * @author David
+ * @version May 16, 2011
+ */
 public class User
 {
   private String my_name;
@@ -8,7 +25,14 @@ public class User
   private StudentPreferences student_preferences;
   private InstructorPreferences instructor_preferences;
   
-  public User(String the_name)
+  /**
+   * Creates a User with the given name.
+   * 
+   * @param the_name The user name.
+   * @throws IllegalArgumentException if the name is null.
+   */
+  public User(final String the_name)
+                           throws IllegalArgumentException
   {
     my_name = the_name; 
     advisor_preferences = new AdvisorPreferences();
@@ -16,6 +40,16 @@ public class User
     instructor_preferences = new InstructorPreferences();   
   }
   
+  /**
+   * <br><br><b>Preconditions:</b>
+   * <ul><li>pre1: a valid Schedule</li>
+   * </ul>
+   * <b>Postconditions:</b>
+   * <ul><li>post1: a list of missing courses</li>
+   * </ul>
+   * @param the_schedule The Schedule of Courses
+   * @return A List of missing Courses
+   */
   public List<Course> getAdvisorFeedback(Schedule the_schedule)
   {
     List<Course> missingCourseList = new ArrayList<Course>();
