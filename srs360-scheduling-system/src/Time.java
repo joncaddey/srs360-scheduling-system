46208e65b8aca373e2080febc4a3f185c677c18a
joncaddey <joncaddey@gmail.com>
2011-05-20 05:05:23 +0000

    Made slight modifications to Time and Timeslot.  not sure whether there should be a static method in time that calculates overlap.

--- srs360-scheduling-system/src/Time.java
+++ srs360-scheduling-system/src/Time.java
@@ -15,11 +15,23 @@
  * </ul>
  * 
  * @author Jonathan Caddey (up to May 19)
+ * @version May 19, 2011 (evening): getLaterTime removed;
+ *          loosened restrictions on 2 arg constructor
+ *          instead to accept negative values.
  * @version May 19, 2011: class created, original signatures
  *          added.
  */
 public class Time implements Comparable<Time>
 {
+
+  /*
+   * first constructor should accept positive or negative
+   * values, whatever range, and do mod or whatever to it to
+   * normalize it (0 <= hour <24, 0 <= min < 60). For
+   * example, it should know what to do with Time(10, 5 -
+   * 15). This makes it easy to see what time 30 minutes
+   * earlier was, for example.
+   */
   public Time(final int the_hours, final int the_minutes)
   {
 
@@ -34,6 +46,16 @@ public class Time implements Comparable<Time>
 
   }
 
+  /*
+   * the_minute_difference should be allowed to be any int;
+   * do modulo to normalize.
+   */
+  public Time(final Time the_time,
+              final int the_minute_difference)
+  {
+
+  }
+
   public int getHour()
   {
     return 0;
@@ -43,10 +65,33 @@ public class Time implements Comparable<Time>
   {
     return 0;
   }
-
-  public static Time getLaterTime(final Time the_time, final int the_added_minutes)
+  
+  /**
+   * Returns whether the two times overlap.
+   * TODO add Description
+   * 
+   * <br>
+   * <br>
+   * <b>Preconditions:</b>
+   * <ul>
+   * <li>TODO</li>
+   * </ul>
+   * <b>Postconditions:</b>
+   * <ul>
+   * <li>TODO</li>
+   * </ul>
+   * @param the_start_1
+   * @param the_end_1
+   * @param the_start_2
+   * @param the_end_2
+   * @param the_difference
+   * @return
+   */
+  public static boolean overlap(final Time the_start_1,
+      final Time the_end_1, final Time the_start_2,
+      final Time the_end_2, final int the_difference)
   {
-    return null;
+
   }
 
   @Override
