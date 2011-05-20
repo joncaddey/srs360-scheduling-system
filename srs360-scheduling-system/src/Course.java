8cc8506b0bfc2d29804556349b6f77746432011d
joncaddey <joncaddey@gmail.com>
2011-05-20 12:50:47 +0000

    combined curriculum code and level into single my_id for Courses to conform with layout of officialScheduleFormat

--- srs360-scheduling-system/src/Course.java
+++ srs360-scheduling-system/src/Course.java
@@ -14,11 +14,14 @@
  * <br>
  * <b>Invariants:</b>
  * <ul>
- * <li>the curriculum code and title are not null.</li>
+ * <li>getTitle() != null</li>
+ * <li>getID() != null</li>
  * <li>getCredits() >= 0</li>
  * </ul>
  * 
  * @author Jonathan Caddey
+ * @version May 20, 2011: combined my_curriculum_code with
+ *          my_level to be expressed by my_id.
  * @version May 19, 2011: added credits field (thank you
  *          Steven Cozart for pointing the need out.)
  * @version May 13, 2011: file created.
@@ -26,15 +29,9 @@
 public class Course
 {
   /**
-   * Example: "TCSS". No number is included.
+   * Example: "TCSS360".
    */
-  private final String my_curriculum_code;
-
-  /**
-   * The number for the course; for example, the 360 in
-   * "TCSS 360".
-   */
-  private final int my_level;
+  private final String my_id;
 
   /**
    * The name of the course, as it appears on the schedule.
@@ -49,30 +46,25 @@ public class Course
 
   /**
    * Creates a Course. For example, in
-   * "TCSS 360 SOFTWARE DEV & QA," "TCSS" is the curriculum
-   * code, 360 is the course level, and "SOFTWARE DEV & QA"
-   * is the title.
+   * "TCSS360 SOFTWARE DEV & QA," "TCSS360" is the course
+   * ID, and "SOFTWARE DEV & QA" is the title.
    * 
-   * @param the_curriculum_code the curriculum code of the
-   *          course.
-   * @param the_level the level of the course.
+   * @param the_id the unique identifier for the course.
    * @param the_title the title of the course.
    * @param the_credits the number of credit hours the
    *          course is worth.
-   * @throws IllegalArgumentException if the_curriculum_code
-   *           or the_title are null, or if the_credits is
-   *           negative.
+   * @throws IllegalArgumentException if the_id or the_title
+   *           are null, or if the_credits is negative.
    */
-  public Course(final String the_curriculum_code,
-                final int the_level,
+  public Course(final String the_id,
                 final String the_title,
                 final int the_credits)
     throws IllegalArgumentException
   {
-    if (the_curriculum_code == null)
+    if (the_id == null)
     {
       throw new IllegalArgumentException(
-        "the_curriculum_code must not be null");
+        "the_id must not be null");
     }
     if (the_title == null)
     {
@@ -84,31 +76,23 @@ public class Course
       throw new IllegalArgumentException(
         "the credits must be positive");
     }
-    my_curriculum_code = the_curriculum_code;
-    my_level = the_level;
+    my_id = the_id;
     my_title = the_title;
     my_credits = the_credits;
   }
 
   /**
-   * @return example: the "TCSS" in "TCSS 360."
+   * @return A unique identifier for the Course. Example:
+   *         "TCSS360".
    */
   public String getCurriculumCode()
   {
-    return my_curriculum_code;
+    return my_id;
   }
 
   /**
-   * @return example: the 360 in "TCSS 360."
-   */
-  public int getLevel()
-  {
-    return my_level;
-  }
-
-  /**
-   * @return the name of the Course, as it appears on the
-   *         Schedule.
+   * @return the descriptive title of the Course, as it
+   *         appears on the Schedule.
    */
   public String getTitle()
   {
