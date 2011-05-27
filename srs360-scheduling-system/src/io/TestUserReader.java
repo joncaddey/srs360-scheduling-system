c4a6180e7c00f1c7cea6229c9095b1cf7cc330e0
joncaddey <joncaddey@gmail.com>
2011-05-27 21:09:55 +0000

    Started UserReader

---
+++ srs360-scheduling-system/src/io/TestUserReader.java
@@ -0,0 +1,22 @@
+/*
+ * Simple Random Sample
+ *
+ * TCSS 360 Dr. Tenenberg
+ *
+ * srs360-scheduling-system
+ */
+package io;
+
+/**
+ * TODO
+ * <br><br><b>Invariants:</b>
+ * <ul><li>TODO invariant1</li>
+ * </ul>
+ * 
+ * @author jcaddey
+ * @version May 27, 2011: Class created.
+ */
+public class TestUserReader
+{
+
+}
---
+++ srs360-scheduling-system/src/io/UserReader.java
@@ -0,0 +1,72 @@
+/*
+ * Simple Random Sample
+ * 
+ * TCSS 360 Dr. Tenenberg
+ * 
+ * srs360-scheduling-system
+ */
+
+package io;
+
+/**
+ * Parses a file containing all the users and their
+ * preferences.<br>
+ * <br>
+ * <b>Invariants:</b>
+ * <ul>
+ * <li>TODO invariant1</li>
+ * </ul>
+ * 
+ * @author Jonathan Caddey
+ * @version May 27, 2011: Class created.
+ */
+public class UserReader
+{
+  
+  /*
+   * user:
+   * 
+   * name:
+   * 
+   * username:
+   * 
+   * password:
+   * 
+   * 
+   * 
+   * 
+   * student:
+   * 
+   * times:
+   * 
+   * days:
+   * 
+   * courses:
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * instructor:
+   * 
+   * times:
+   * 
+   * days:
+   * 
+   * courses:
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * advisor:
+   * 
+   * courses:
+   * 
+   * scheduler:
+   */
+
+}
