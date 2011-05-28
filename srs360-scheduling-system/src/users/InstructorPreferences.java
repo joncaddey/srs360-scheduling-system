1e1146a0ae5c89c0cc570e154f85e4e1054a4f05
david.liddington@gmail.com <david.liddington@gmail.com@51c3290b-bf2c-f903-1b12-c83ff7bf9553>
2011-05-28 04:01:01 +0000

    User.getStudPref should be ok for Greg to use now.

--- srs360-scheduling-system/src/users/InstructorPreferences.java
+++ srs360-scheduling-system/src/users/InstructorPreferences.java
@@ -1,11 +1,14 @@
 package users;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import courses.Course;
 import courses.Day;
 import courses.GeneralTime;
+import courses.Schedule;
 import courses.Section;
 
 /**
@@ -85,6 +88,65 @@ public class InstructorPreferences
   public int getMaxCreditHours()
   {
     return my_max_credit_hours;
+  }
+  
+  /**
+   * For every course this instructor wants to teach, this
+   * method returns the day and time he wants to teach it,
+   * if the schedule does not already provide that.
+   * <br>
+   * <br>
+   * <b>Preconditions: the Schedule.</b>
+   * <ul>
+   * <li>TODO</li>
+   * </ul>
+   * <b>Postconditions: a Collection of responses, or 
+   * feedbacks, from the instructor, each of which has a
+   * course the instructor wants to teach and the day and
+   * time he wants to teach it.</b>
+   * <ul>
+   * <li>TODO</li>
+   * </ul>
+   * @param the_schedule The Schedule
+   * @return The Collection of instructor feedbacks
+   */     
+  public Collection<InstructorFeedback> getInstructorFeedback
+                               (final Schedule the_schedule)
+  {
+    Collection<InstructorFeedback> instructor_feedback_collection
+                      = new ArrayList<InstructorFeedback>();
+    List<Section> preferred_sections =
+               instructor_preferences.getPreferredSections();
+    List<Day> preferred_days =
+                  instructor_preferences.getPreferredDays();
+    List<GeneralTime> preferred_times =
+          instructor_preferences.getPreferredGeneralTimes();
+ 
+    for(Course each_course : preferred_courses)
+    {
+      if(!the_schedule.hasSection(each_course,
+                                  preferred_days,
+                                  preferred_times))
+      {
+        for(Day each_day : Day.getAllDays())
+        {
+          for(GeneralTime each_time :
+                           GeneralTime.getAllGeneralTimes())
+          {
+            if(preferred_days.contains(each_day) &&
+               preferred_times.contains(each_time))
+            {
+              InstructorFeedback instructor_feedback =
+              new InstructorFeedback(each_course, each_day,
+                                                 each_time);            
+              instructor_feedback_collection.add
+                                      (instructor_feedback);
+            }
+          }
+        }     
+      }
+    } 
+    return instructor_feedback_collection;   
   }
   
 }
