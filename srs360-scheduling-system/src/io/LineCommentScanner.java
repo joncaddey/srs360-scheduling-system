7e975adb248da7aff795d79d0ac7030862b3a35b
joncaddey <joncaddey@gmail.com>
2011-05-29 02:39:30 +0000

    going to trim down what I've made so I make fewer promises

---
+++ srs360-scheduling-system/src/io/LineCommentScanner.java
@@ -0,0 +1,94 @@
+/*
+ * Simple Random Sample
+ * 
+ * TCSS 360 Dr. Tenenberg
+ * 
+ * srs360-scheduling-system
+ */
+
+package io;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+/**
+ * A wrapper for Scanner which can be used to keep track of
+ * line number and skip commented and blank lines. <br>
+ * <br>
+ * <b>Invariants:</b>
+ * <ul>
+ * <li>TODO invariant1</li>
+ * </ul>
+ * 
+ * @author Jonathan Caddey
+ * @version May 28, 2011: Class created.
+ */
+public class LineCommentScanner
+{
+  private final String COMMENT_STRING = "%";
+  final private Scanner my_scanner;
+  private int my_line_number;
+  private String my_last;
+
+  public LineCommentScanner(final Scanner the_scanner)
+  {
+    my_scanner = the_scanner;
+    my_line_number = 1;
+  }
+
+  /**
+   * Advances the Scanner until the first line after the
+   * current which is not blank and does not start with a
+   * "%". <br>
+   * <br>
+   * <b>Preconditions:</b>
+   * <ul>
+   * <li>none</li>
+   * </ul>
+   * <b>Postconditions:</b>
+   * <ul>
+   * <li>will not return null</li>
+   * </ul>
+   * 
+   * @return the next line from the given Scanner which is
+   *         not blank and does not start with "%".
+   * @throws NoSuchElementException if end of file is
+   *           reached before a non-commented line.
+   */
+  public String getNonComment()
+      throws NoSuchElementException
+  {
+    String non = null;
+    while (my_scanner.hasNext())
+    {
+      final String line = my_scanner.nextLine();
+      my_line_number++;
+      if (!"".equals(line.trim()) &&
+          !line.startsWith(COMMENT_STRING))
+      {
+        non = line;
+        break;
+      }
+    }
+    if (non == null)
+    {
+      throw new NoSuchElementException(
+        "Expected a non-comment line, "
+            + "but reached end of file");
+    }
+    return non;
+  }
+  
+  
+
+  public int getLineNumber()
+  {
+    return my_line_number;
+  }
+
+  public void setLineNumber(final int the_line_number)
+  {
+    my_line_number = the_line_number;
+  }
+
+}
--- srs360-scheduling-system/src/io/UserReader.java
+++ srs360-scheduling-system/src/io/UserReader.java
@@ -8,9 +8,65 @@
 
 package io;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Scanner;
+
+import users.User;
+
 /**
+ * <p>
  * Parses a file containing all the users and their
- * preferences.<br>
+ * preferences. The file should contain the information of
+ * all users. Empty lines or lines starting with the '%'
+ * character are skipped by the reader. Information is
+ * organized using case-insensitive tags.
+ * </p>
+ * <p>
+ * The representation of a new user on the file starts with
+ * the "USER" tag on its own line. The next 3 lines are
+ * interpreted as the user name, user id, and password.
+ * </p>
+ * <p>
+ * If the user is a Student, the "STUDENT" tag should follow
+ * on its own line. The next line should contain a list of
+ * days the student would be willing to go to school (see
+ * TODO DaySlotReader.parseDayString()). The line after that
+ * should be a comma-separated list of times of day the
+ * student would be willing to go to school, being either
+ * "MORNING", "EVENING", or "MORNING,EVENING". Finally, the
+ * next line should be a comma-separated list of course IDs
+ * of Courses the Student would like to attend.
+ * </p>
+ * 
+ * <p>
+ * If the user is a Instructor, the "INSTRUCTOR" tag should
+ * follow on its own line. The next line should contain a
+ * list of days the instructor would be willing to teach
+ * (see TODO DaySlotReader.parseDayString()). The line after
+ * that should be a comma-separated list of times of day the
+ * instructor would be willing to teach, being either
+ * "MORNING", "EVENING", or "MORNING,EVENING". Finally, the
+ * next line should be a comma-separated list of course IDs
+ * of Courses the Instructor would be willing to teach.
+ * </p>
+ * 
+ * <p>
+ * If the user is a Advisor, the "ADVISOR" tag should follow
+ * on its own line. The next line should be a
+ * comma-separated list of course IDs of Courses the Advisor
+ * recommends be on the Schedule.
+ * </p>
+ * 
+ * <p>
+ * If the user is a Scheduler, the "SCHEDULER" tag should be
+ * on its own line.
+ * </p>
+ * 
+ * 
+ * <br>
  * <br>
  * <b>Invariants:</b>
  * <ul>
@@ -22,51 +78,182 @@ package io;
  */
 public class UserReader
 {
-  
-  /*
-   * user:
-   * 
-   * name:
-   * 
-   * username:
-   * 
-   * password:
-   * 
-   * 
-   * 
-   * 
-   * student:
-   * 
-   * times:
-   * 
-   * days:
-   * 
-   * courses:
-   * 
-   * 
-   * 
-   * 
-   * 
-   * 
-   * 
-   * instructor:
-   * 
-   * times:
-   * 
-   * days:
-   * 
-   * courses:
-   * 
-   * 
-   * 
-   * 
-   * 
-   * 
-   * advisor:
-   * 
-   * courses:
-   * 
-   * scheduler:
-   */
 
+  /**
+   * Lines starting with this String are ignored by the
+   * reader. Blank lines are also ignored.
+   */
+  private static final String COMMENT_STRING = "%";
+
+  /**
+   * Indicates the next lines from file will concern a new
+   * user.
+   */
+  private static final String USER_TAG = "USER";
+
+  /**
+   * Indicates the next lines from file will concern the
+   * Student preferences.
+   */
+  private static final String STUDENT_TAG = "STUDENT";
+
+  /**
+   * Indicates the next lines from file will concern the
+   * Instructor preferences.
+   */
+  private static final String INSTRUCTOR_TAG = "INSTRUCTOR";
+
+  /**
+   * Indicates the next lines from file will concern the
+   * Advisor preferences.
+   */
+  private static final String ADVISOR_TAG = "ADVISOR";
+
+  /**
+   * Indicates the user is a Scheduler.
+   */
+  private static final String SCHEDULER_TAG = "SCHEDULER";
+
+  /**
+   * A mapping of user IDs to Users.
+   */
+  private Map<String, User> my_user_map;
+
+  // constructor needs as an argument something that can
+  // parse Strings, and possibly a list of all Courses.
+
+  // Should have read(File)
+
+  /**
+   * Reads a given file containing information about all the
+   * users. See class documentation on how the file should
+   * be organized.
+   * 
+   * <br>
+   * <br>
+   * <b>Preconditions:</b>
+   * <ul>
+   * <li>TODO</li>
+   * </ul>
+   * <b>Postconditions:</b>
+   * <ul>
+   * <li>TODO</li>
+   * </ul>
+   * 
+   * @param the_file contains the user data in the correct
+   *          format.
+   * 
+   */
+  public void read(final File the_file) throws IOException
+  {
+    final LineCommentScanner scanner =
+        new LineCommentScanner(new Scanner(the_file));
+    String tag = scanner.getNonComment();
+    if (tag != null)
+    {
+      tag = tag.toUpperCase();
+    }
+    while (USER_TAG.equals(tag))
+    {
+      final String user_name = scanner.getNonComment();
+
+      System.out.println("USER ID: " +
+                         scanner.getNonComment());
+      System.out.println("PASSWORD: " +
+                         scanner.getNonComment());
+
+      tag = scanner.getNonComment();
+      if (tag != null)
+      {
+        tag = tag.toUpperCase();
+      }
+
+      while (tag != null && !USER_TAG.equals(tag))
+      {
+        if (tag.equals(STUDENT_TAG))
+        {
+          System.out.println("STUDENT");
+        }
+        else if (tag.equals(INSTRUCTOR_TAG))
+        {
+          System.out.println("INSTRUCTOR");
+        }
+        else if (tag.equals(ADVISOR_TAG))
+        {
+          System.out.println("ADVISOR");
+        }
+        else if (tag.equals(SCHEDULER_TAG))
+        {
+          System.out.println("ADVISOR");
+        }
+        tag = scanner.getNonComment();
+        if (tag != null)
+        {
+          tag = tag.toUpperCase();
+        }
+
+      }
+    }
+
+  }
+
+  // Should have Map<String, User> getUserMap, which returns
+  // mapping of userId to Users.
+
+  /**
+   * Returns a mapping of user IDs to the Users they belong
+   * to. The Users are those represented in the file passed
+   * to the last successful read invocation.
+   * 
+   * <br>
+   * <br>
+   * <b>Preconditions:</b>
+   * <ul>
+   * <li>read(File) has been called</li>
+   * </ul>
+   * <b>Postconditions:</b>
+   * <ul>
+   * <li>TODO</li>
+   * </ul>
+   * 
+   * @return a mapping of user IDs to the Users they belong
+   *         to.
+   */
+  Map<String, User> getUserMap()
+  {
+    return Collections.unmodifiableMap(my_user_map);
+  }
+
+  public static void main(String[] args)
+  {
+    // try
+    // {
+    // new UserReader().read(new File(
+    // "src/io/testUserReaderFile.txt"));
+    // }
+    // catch (IOException the_e)
+    // {
+    // System.out.println(the_e.getMessage());
+    // }
+    Scanner scanner;
+    try
+    {
+      String separator = System
+      .getProperty("line.separator");
+      scanner =
+          new Scanner(new File(
+            "src/io/testUserReaderFile.txt"))
+              .useDelimiter("[(aa)(bb)]");//%[^\n]+\n+
+
+      System.out.println(scanner.next());
+      System.out.println(scanner.next());
+      System.out.println(scanner.next());
+      System.out.println(scanner.next());
+
+    }
+    catch (IOException the_e)
+    {
+      System.out.println(the_e.getMessage());
+    }
+  }
 }
